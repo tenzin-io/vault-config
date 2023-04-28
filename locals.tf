a13de733e4ada96fc0d938bfe074419c5cc95833
Tenzin Lhakhang <2402007+tlhakhan@users.noreply.github.com>
2023-04-28 14:56:56 -0400

    Add repo

--- locals.tf
+++ locals.tf
@@ -1,5 +1,9 @@
 locals {
   allowed_github_repos = {
+    "actions-runner" = {
+      claim          = "repo:tenzin-io/actions-runner:ref:refs/heads/main"
+      token_policies = [vault_policy.read_secrets.name]
+    }
     "cloudflare-dns" = {
       claim          = "repo:tenzin-io/cloudflare-dns:ref:refs/heads/main"
       token_policies = [vault_policy.read_secrets.name]
