61f4441c6ca8f99ea6003350c8d812d75a4f8c40
Tenzin Lhakhang <2402007+tlhakhan@users.noreply.github.com>
2023-06-23 23:11:48 -0400

    Update variable descriptions

--- variables.tf
+++ variables.tf
@@ -1,17 +1,17 @@
 variable "vault_admin_password" {
   type        = string
   sensitive   = true
-  description = "The Vault admin password."
+  description = "The Vault admin password"
 }
 
 variable "github_secret_mount_path" {
   type        = string
-  description = "The secret store mount path"
+  description = "The mount path for the github secret store"
   default     = "github"
 }
 
 variable "github_repo_to_secret_paths" {
   type        = map(list(string))
   default     = {}
-  description = "A map of GitHub repo to allowed secret paths under the github_secret_mount_path"
+  description = "A map of GitHub repo to its allowed secret paths under the github secret store"
 }
