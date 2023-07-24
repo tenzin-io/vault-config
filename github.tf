bf76f3582ce682c4280c10ba92b35faa620e11db
Tenzin Lhakhang <2402007+tlhakhan@users.noreply.github.com>
2023-07-23 20:56:23 -0400

    Slashes not allowed in policy or role names

--- github.tf
+++ github.tf
@@ -12,7 +12,7 @@ resource "vault_jwt_auth_backend_role" "github_repos" {
   for_each       = var.github_repos
   backend        = vault_jwt_auth_backend.github.path
   role_name      = "actions-runner-role-${each.key}"
-  token_policies = flatten([vault_policy.default_policy.name, [for p in each.value.secrets : format("github-repo-to-secrets-policy-%s", p)]])
+  token_policies = flatten([vault_policy.default_policy.name, [for p in each.value.secrets : format("github-repo-to-secrets-policy-%s", md5(p))]])
   bound_claims = {
     repository = each.key
   }
@@ -23,7 +23,7 @@ resource "vault_jwt_auth_backend_role" "github_repos" {
 
 resource "vault_policy" "github_repo_to_secrets" {
   for_each = toset(distinct(flatten([for k, v in var.github_repos : v.secrets])))
-  name     = "github-repo-to-secrets-policy-${each.value}"
+  name     = "github-repo-to-secrets-policy-${md5(each.value)}"
   policy   = <<-EOT
     path "${each.value}" {
       capabilities = ["read"]
