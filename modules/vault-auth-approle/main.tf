ec659edc171af8acedfd90f1381f70f1e901a9e2
Tenzin Lhakhang <2402007+tlhakhan@users.noreply.github.com>
2024-07-20 15:48:20 +0000

    Fix custom role_id

--- modules/vault-auth-approle/main.tf
+++ modules/vault-auth-approle/main.tf
@@ -18,6 +18,7 @@ resource "vault_approle_auth_backend_role" "app" {
   for_each              = var.allowed_apps
   backend               = vault_auth_backend.approle.path
   role_name             = each.key
+  role_id               = each.key
   token_policies        = each.value.policies
   bind_secret_id        = false
   secret_id_bound_cidrs = concat(var.global_bound_cidrs, each.value.bound_cidrs)
@@ -26,5 +27,5 @@ resource "vault_approle_auth_backend_role" "app" {
 resource "vault_approle_auth_backend_login" "login" {
   for_each = var.allowed_apps
   backend  = vault_auth_backend.approle.path
-  role_id  = each.key
+  role_id  = vault_approle_auth_backend_role.app[each.key].role_id
 }
