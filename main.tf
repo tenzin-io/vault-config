9db85997b910e0bbc2ac53c3ceda023c9bf176b5
Tenzin Lhakhang <2402007+tlhakhan@users.noreply.github.com>
2025-02-17 17:51:03 +0000

    Update policy for tenzin-bot

--- main.tf
+++ main.tf
@@ -32,6 +32,7 @@ module "vault_policies" {
     ],
     "tenzin-bot" = [
       { path = "secrets/data/docker-hub/tenzinbot", capabilities = ["read", "list"] },
+      { path = "secrets/data/cloudflare/tunnels/*", capabilities = ["read", "list"] },
       { path = "kubernetes-secrets/data/kubeconfig/*", capabilities = ["create", "update"] },
       { path = "auth/token/create", capabilities = ["create", "read", "update", "list"] },
     ]
